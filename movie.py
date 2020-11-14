78da97916cf93d1d9fc64f36685050f593f42b51
juah <skyblue_@naver.com>
2020-11-14 13:14:04 +0900

    2주차 과제 계획

--- movie.py
+++ movie.py
@@ -6,6 +6,23 @@ while True:
     if menu == '1':
         #1 선택시 영화 리스트 생성(2주차)
         print("1번을 선택하셨습니다.")
+        f = open('movielist.txt', 'rt', encoding='UTF8')
+        movieNum = 1
+        movieNameList = []
+        while(1):
+            M = f.readline()
+            if '\n' in M:
+                M = M.replace('\n','')
+            if M == '':
+                break
+            print(movieNum,".", M)
+            movieNameList.append(M)
+            movieNum = movieNum + 1
+        i = input("숫자를 입력해 영화를 선택하세요 : ")
+        i = int(i)
+        moviename = movieNameList[i - 1]
+        print(i, "번", moviename, "을 선택하셨습니다.")
+        break
     elif menu == '2':
         print("프로그램을 종료합니다.")
         break
